4afab4f682d07832e6ff55d72f4e4f9c4d18442b
Richard Perkins <professorluticor@gmail.com>
2023-04-29 21:58:05 -0400

    Most .py functions now working.

---
+++ custom_functions.py
@@ -0,0 +1,88 @@
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset 
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def image_to_dataframe(img):
+    '''
+    This function takes in a PIL grayscale Image using:
+        img = Image.open('{path_to_image}').convert('L')
+    Then it converts the pixels into datapoints in a Pandas dataframe.
+
+    Args:
+        img (PIL.Image): Grayscale Image object from PIL
+
+    Returns:
+        df (pd.DataFrame): Dataframe of normalized pixel coordinates and normalized pixel values. 
+            All values will be between 0 and 1. The vertical axis will be inverted with the bottom at 0 and top at 1.
+    '''
+    pixels = img.getdata()
+    width, height = img.size
+    #Extract the individual coordinates and pixel values as a list
+    x_coords = []
+    y_coords = []
+    intensity = []
+    for i in range(height):
+        for j in range(width):
+            x_coords.append(j)
+            y_coords.append(height-i) #Flip y axis so bottom is 0 top is highest value
+            intensity.append(pixels[i*width + j])
+    # Assemble the Dataframe after turning the lists into arrays and normalizing by the maximum values
+    df = pd.DataFrame({'x': np.array(x_coords)/width, 'y': np.array(y_coords)/height, 'z': np.array(intensity)/255.})
+    return df
+
+def df_to_dataloader(df, batch_size = 1024, shuffle=True, verbose=True):
+    '''
+    Converts dataframe of points to a Torch Dataloader.
+    
+    Args:
+        df(pd.DataFrame): Dataframe of normalized pixel coordinates and normalized pixel values. All values will be between 0 and 1.
+        batch_size (int): Batchsize for the dataloader and neural network training
+        verbose (bool): Flag to allow print statements
+    
+    Returns:
+        dataloader (torch.utils.data.Dataloader): A PyTorch dataloader that iterates through the data for training.
+    '''
+    #Convert DataFrame to tensors
+    x_tensor = torch.tensor(df.iloc[:,0:2].values, dtype=torch.float)
+    y_tensor = torch.tensor(df.iloc[:,-1].values, dtype=torch.float)
+
+    # Create an instance of the dataset and dataloader
+    dataset = TensorDataset(x_tensor, y_tensor)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+
+    if verbose:
+        print('Batches per Epoch:',len(dataloader))
+        print('x_tensor.shape', x_tensor.shape)
+        print('y_tensor.shape', y_tensor.shape)
+
+    return dataloader
+
+
+def plot_performance(net, scale=8, batch_size=64, width = 1920, height = 1080):
+    #Custom function to plot results of image learning.
+    xlin = np.linspace(0, 1, width // scale)
+    ylin = np.linspace(0, 1, height // scale)
+    xv, yv = np.meshgrid(xlin, ylin)
+
+    xv_tensor = torch.tensor(xv, dtype=torch.float)
+    yv_tensor = torch.tensor(yv, dtype=torch.float)
+
+    reduced_tensor = torch.stack((xv_tensor, yv_tensor), dim=-1)
+    reduced_dataset = TensorDataset(reduced_tensor, torch.zeros(size=(len(reduced_tensor), 1)))
+    reduced_dataloader = DataLoader(reduced_dataset, batch_size=batch_size, shuffle=False)
+
+    with torch.no_grad():
+        output_list = []
+        for x_batch, _ in reduced_dataloader:
+            outputs = net(x_batch)
+            output_list.append(outputs)
+
+        outputs = torch.cat(output_list, dim=0).numpy()
+        
+    plt.figure(figsize=(12, 8))
+    plt.imshow(outputs, cmap='Greys_r', origin='lower')
+    plt.show()
+
---
+++ trainer.py
@@ -0,0 +1,103 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+import matplotlib
+import os
+import pickle
+import datetime
+
+# if "DISPLAY" not in os.environ:
+#     matplotlib.use("Agg")  # Use non-interactive backend for scripts running without a display
+# else:
+#     matplotlib.use("notebook")  # Use interactive backend for Jupyter notebooks
+import matplotlib.pyplot as plt
+
+import model
+
+class NeuralNetworkTrainer:
+    def __init__(self, dataloader, net=model.Net(nn_shape=(2,4,50,1)), n_epochs=20, lr=10**(-2.3), lr_schedule='auto', plot_func=None):
+        self.dataloader = dataloader
+        self.net = net
+        self.n_epochs = n_epochs
+        self.lr = lr
+        self.lr_exp = np.log10(lr)
+        self.lr_schedule = lr_schedule
+        self.plot_func = plot_func
+        self.criterion = nn.MSELoss()
+        self.optimizer = optim.Adam(self.net.parameters(), lr=lr)
+        self.loss_values = []
+
+    def train(self, plot=True):
+        for epoch in range(self.n_epochs):
+            if self.lr_schedule=='auto' or self.lr_schedule is None:
+                for g in self.optimizer.param_groups:
+                    g['lr'] = 10**(self.lr_exp - 2.*(epoch/(self.n_epochs-1)))
+            elif  self.lr_schedule=='fixed':
+                pass
+            print('Epoch', epoch, 'of', self.n_epochs)
+            loop = tqdm(self.dataloader, leave=True)
+            for x_batch, y_batch in loop:
+                # Forward pass
+                outputs = self.net(x_batch)
+                loss = self.criterion(outputs, y_batch.unsqueeze(dim=-1))
+                # Backward pass and optimization
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                # Store the loss value
+                self.loss_values.append(loss.item())
+
+            # Print the loss
+            print(f"Loss = {self.loss_values[-1]}")
+            if plot:
+                # Show current network progress
+                if epoch in [0,1,2,4,7,10,20,40]:
+                    self.plot_output()
+
+    def plot_output(self):
+        # Call plot function if it is defined
+        if self.plot_func is not None:
+            self.plot_func(self.net)
+        else:
+            print("self.plot_func is undefined.")
+
+
+    def plot_loss(self):
+        epochs_values = np.array(range(len(self.loss_values))) / len(self.dataloader)
+        plt.plot(epochs_values, self.loss_values)
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.yscale('log')
+        plt.grid()
+        plt.show()
+
+    def save_model(self, file_path):
+        torch.save(self.net.state_dict(), file_path)
+
+    def save_trainer(self, file_path=None):
+        if file_path is None:
+            now = datetime.datetime.now()
+            date_string = now.strftime("%m%d_%H%M")
+            file_path = f"working/model_{date_string}.pickle"
+        with open(file_path, 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def load_trainer(cls, file_path):
+        with open(file_path, 'rb') as f:
+            return pickle.load(f)
+
+
+'''
+To use the modules use the following commands:
+
+nn_trainer = NeuralNetworkTrainer(dataloader, net=model.Net, n_epochs=20, lr=10**(-2.3), plot_func=None)
+nn_trainer.train()
+nn_trainer.plot_output()
+nn_trainer.plot_loss()
+nn_trainer.save_trainer()
+nn_trainer = NeuralNetworkTrainer.load_trainer(file_path={filepath})
+'''
